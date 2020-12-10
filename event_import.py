e84753727ad1d37bb4984c2608b34c28db8ecd58
Ryan Moore <kiwi.ryanmoore@gmail.com>
2020-12-10 23:39:48 +1300

    event import complete

--- event_import.py
+++ event_import.py
@@ -7,16 +7,15 @@ from dateutil.parser import parse
 import datetime
 import logging
 from os import system
-import pprint
+
+logging.basicConfig(level="INFO")
 
 def field_repr(*args):
     rep_arr = [field.__repr__() for field in args]
     return f"({', '.join(rep_arr)})"
-
-class Event_Types(Enum):
-    pass
-
-csv_path = sys.argv[1]
+def clear():
+    system("cls")
+    system("clear")
 
 cnx = mysql.connector.connect(
     host="127.0.0.1",
@@ -24,12 +23,8 @@ cnx = mysql.connector.connect(
     user="root",
     password="admin")
 
-def clear():
-    system("cls")
-    system("clear")
-
-commit = False
-logging.basicConfig(level="INFO")
+csv_path = sys.argv[1]
+commit = True
 
 cursor = cnx.cursor(dictionary=True)
 
@@ -88,7 +83,6 @@ clear()
 
 logging.info("adding event to record...")
 cursor.execute(f"INSERT INTO mydb.event VALUES {field_repr(season, number, name, date, event)}")
-if commit: cnx.commit()
 
 event_grades = []
 logging.info("creating event grades...")
@@ -97,7 +91,6 @@ with open(csv_path, "r") as csvfile:
     for row in reader:
         if row["Short"] and row["Short"] not in event_grades:
             event_grades.append(row["Short"])
-
 
 cursor.execute(f"SELECT * FROM mydb.grade WHERE season_idyear = {season}")
 grades = [row["idgrade"].upper() for row in cursor.fetchall()]
@@ -112,25 +105,46 @@ for grade in grades:
 
 for event_grade in event_grades:
     cursor.execute(f"INSERT INTO mydb.event_grades VALUES {field_repr(event_grade, season, number)}")
-if commit: cnx.commit()
 
 cursor.execute(f"SELECT * FROM mydb.member")
 members = [row["first_name"] + "%" + row["last_name"] for row in cursor.fetchall()]
 
 def get_details(member):
     first, last = member.split("%")
-    cursor.execute(f"SELECT DOB, gender FROM mydb.member WHERE first_name = %s AND last_name = %s", (first, last)) 
+    cursor.execute(f"SELECT idmember, DOB, gender FROM mydb.member WHERE first_name = %s AND last_name = %s", (first, last)) 
     a = cursor.fetchall()[0]
-    return a["DOB"], a["gender"] 
+    return a["idmember"], a["DOB"], a["gender"] 
 
 non_members = []
 
+def import_result(member_id, result):
+    time = result["Time"]
+    time = [int(time) for time in time.split(":")]
+    time = [0] * (3 - len(time)) + time
+
+    cursor.execute(f"""INSERT INTO mydb.result (
+        member_idmember,
+        event_grades_event_season_idyear,
+        event_grades_event_num,
+        event_grades_grade_idgrade, 
+        time,
+        points
+        ) VALUES (%s, %s, %s, %s, %s, %s)""", 
+        (
+            member_id,
+            season,
+            number,
+            result["Short"],
+            datetime.time(*time),
+            None
+        ))
+
 with open(csv_path, "r") as csvfile:
     reader = csv.DictReader(csvfile)
-    for competitor in reader:
-        competitor_dob = competitor["YB"]
-        competitor_gender = competitor["S"]
-        competitor = competitor["First name"] + "%" + competitor["Surname"]
+    for field in reader:
+        competitor_dob = field["YB"]
+        competitor_gender = field["S"]
+        competitor = field["First name"] + "%" + field["Surname"]
         result = process.extract(competitor, members, limit=3, scorer=fuzz.token_sort_ratio)
         competitor = competitor.replace("%", " ")
         if result[0][1] == 100:
@@ -139,38 +153,42 @@ with open(csv_path, "r") as csvfile:
                 logging.error(f"OMITTING:multiple members with name {competitor} found")
             else:
                 member = result[0][0]
-                member_dob, member_gender = get_details(member)
+                member_id, member_dob, member_gender = get_details(member)
                 member = member.replace("%", " ")
                 if competitor_dob and competitor_gender:
                     if member_dob.year == int(competitor_dob) and member_gender == competitor_gender:
-                        pass # here
+                        import_result(member_id, field)
                     else:
                         logging.warning(f"ADDING:details of {competitor} do not match with the members database")
-                        # pass
+                        import_result(member_id, field)
 
                 else:
                     # logging.info(f"found competitor {competitor} but their gender or DOB is missing, ADDING")
-                    # here
+                    import_result(member_id, field)
                     pass
         elif result[0][1] > 70:
             for re in result:
                 member = re[0]
-                member_dob, member_gender = get_details(member)
+                member_id, member_dob, member_gender = get_details(member)
                 member = member.replace("%", " ")
                 if competitor_dob:
                     if member_dob.year == int(competitor_dob) and member_gender == competitor_gender:
                         if re[1] >= 90:
-                            # here
+                            import_result(member_id, field)
                             pass
                         else:
                             logging.warning(f"ADDING:match between {competitor} and {member} is not certain, but DOB and gender match")
-                            # here
+                            import_result(member_id, field) 
                         break
                 else:
                     if re[1] >= 90:
                         if member_gender == competitor_gender:
                             logging.warning(f"ADDING:match between {competitor} and {member} is not certain, but gender matches")
+                            import_result(member_id, field)
                             break
+                        else:
+                            logging.warning(f"OMITTING:match between {competitor} and {member} is likely but all details are different")
+
             else:
                 non_members.append(
                     [competitor.ljust(25),
@@ -184,14 +202,18 @@ with open(csv_path, "r") as csvfile:
             [competitor.ljust(25),
             f"{result[0][1]}%",
             f"{competitor_gender}",
-            f"DOB:{competitor_dob}".ljust(9),
-            f"c:None"])
+            f"DOB:{competitor_dob}".ljust(9)])
 
 non_members.sort(key=lambda x: x[1], reverse=True)
 logging.info(f"non-members omitted in this import:")
 print(*(' '.join(row) for row in non_members), sep='\n')
 print()
+
 i = input("press enter to continue import: ")
 if i == "n" or i == "no":
     print("aborting...")
     sys.exit()
+
+logging.info("importing results...")
+logging.info("done!")
+if commit: cnx.commit()
