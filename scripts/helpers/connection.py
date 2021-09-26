0418a8163472b42655bcab6ab6d5805f5e2d647a
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-09-26 18:43:40 +1300

    add member import

---
+++ scripts/helpers/connection.py
@@ -0,0 +1,28 @@
+"""Define cursor object so the other scripts can connect to the database."""
+import logging
+from os import environ
+
+from mysql import connector  # type: ignore
+
+uri_args = {
+    "host": "127.0.0.1",
+    "port": "3306",
+    "user": "root",
+    "password": "",
+}
+
+for key in uri_args:
+    uri_value = environ.get(f"MYSQL_{key.upper()}")
+    if uri_value:
+        uri_args[key] = uri_value
+
+connection = connector.connect(**uri_args)
+cursor = connection.cursor()
+
+
+def commit_and_close() -> None:
+    """Commit the cursor changes and close the connection."""
+    logging.info("committing changes...")
+    connection.commit()
+    connection.close()
+    logging.info("done")
