470875ea61dc1e0cba41c7dd6111219e57e9e9e5
Ryan Moore <kiwi.ryanmoore@gmail.com>
2021-10-01 18:41:56 +1300

    create event

---
+++ scripts/create_event.py
@@ -0,0 +1,77 @@
+import logging
+from os import path
+from sys import argv, stdin
+
+from dateutil import parser
+from helpers.connection import commit_and_close, cursor
+
+MAX_EVENTS = 10
+
+logging.basicConfig(level=logging.INFO, format="")
+csv_path = path.join(path.dirname(__file__), "..", argv[1])
+
+cursor.execute("SELECT year FROM oypoints.season")
+seasons = cursor.fetchall()
+cursor.execute("SELECT * FROM oypoints.discipline")
+disciplines = cursor.fetchall()
+
+logging.info("available seasons:")
+for season in seasons:
+    logging.info(season[0])
+while True:
+    logging.info("season?")
+    try:
+        season = int(stdin.readline())
+    except ValueError:
+        logging.info("that is not a valid season.")
+    else:
+        if season in {field[0] for field in seasons}:
+            break
+        logging.info("that is not a valid season.")
+
+
+while True:
+    logging.info("event # in series?")
+    try:
+        number = int(stdin.readline())
+    except ValueError:
+        logging.info("that is not a valid event #.")
+    else:
+        if 0 < number < MAX_EVENTS:
+            break
+        logging.info("that is not a valid event #.")
+
+while True:
+    logging.info("event name?")
+    name = stdin.readline().strip()
+    if name:
+        break
+
+logging.info("available disciplines:")
+for discipline in disciplines:
+    logging.info(f"{discipline[0]}: {discipline[1]}")
+while True:
+    logging.info("discipline code?")
+    event_disc = stdin.readline().strip().upper()
+    if event_disc in {discipline[0].upper() for discipline in disciplines}:
+        break
+    else:
+        logging.info("that is not a valid discipline.")
+
+while True:
+    logging.info("event date?")
+    try:
+        date = parser.parse(stdin.readline()).strftime("%Y-%m-%d")
+    except parser.ParserError:
+        logging.info("that is not a valid date.")
+    else:
+        break
+
+logging.info("adding event to record...")
+cursor.execute(
+    "REPLACE INTO oypoints.event "
+    "VALUES (%s, %s, %s, %s, %s)",
+    [season, number, name, date, event_disc],
+)
+
+commit_and_close()
--- scripts/member_import.py
+++ scripts/member_import.py
@@ -1,12 +1,11 @@
-from helpers.connection import commit_and_close, cursor
-from csv import DictReader
 import logging
 import sys
+from csv import DictReader
 from os import path
 
-logging.basicConfig(level=20)
+from helpers.connection import commit_and_close, cursor
 
-
+logging.basicConfig(level=logging.INFO, format="")
 csv_path = path.join(path.dirname(__file__), "..", sys.argv[1])
 
 fieldname_mapper = {
@@ -17,14 +16,20 @@ fieldname_mapper = {
     "gender": "Gender",
 }
 
-with open(csv_path, "r") as f:
-    for row in DictReader(f):
+with open(csv_path, "r") as members_csv:
+    for row in DictReader(members_csv):
         row_data = [
-            row[fieldname] for fieldname in fieldname_mapper.values()]
-        logging.info(f"adding member {row['First name']} {row['Last name']}")
+            row[fieldname] for fieldname in fieldname_mapper.values()
+        ]
+        logging.info("adding member "
+                     f"{row['First name']} {row['Last name']}",
+                     )
+
+        keys = ",".join(fieldname_mapper.keys())
         cursor.execute(
-            f"REPLACE INTO oypoints.member "
-            f"({','.join(fieldname_mapper.keys())}) VALUES (%s, %s, %s, %s, %s)",
-            row_data)
+            "REPLACE INTO oypoints.member "
+            f"({keys}) VALUES (%s, %s, %s, %s, %s)",
+            row_data,
+        )
 
 commit_and_close()
