592fdcc0b17e2fea258d8b8da231fd4457776ca6
Michael Smithson <michael@smithson.co.nz>
2022-02-09 22:27:27 +1300

    Update database name to match api.

--- api/src/api.js
+++ api/src/api.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
   host: process.env.MYSQL_HOST || "127.0.0.1",
   user: process.env.MYSQL_USER || "root",
   password: password,
-  database: process.env.MYSQL_DB || "oypoints",
+  database: "oypoints",
 });
 
 connection.connect();
